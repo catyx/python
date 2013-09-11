3f3a85f4f032155f3899612701a29af84830d388
catyx <catalina.taran@gmail.com>
2013-09-11 19:36:21 -0400

    First exercise in Py

--- first.py
+++ first.py
@@ -1,20 +1,8 @@
-x = "There are %d types of people." % 10
-binary = "binary"
-do_not = "don't"
-y = "Those who know %s and those who %s." % (binary, do_not)
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
 
-print(x)
-print(y)
-
-print("I said: %r." % x)
-print("I also said: '%s'." % y)
-
-hilarious = False
-joke_evaluation = "Isn't that joke so funny?! %r"
-
-print(joke_evaluation % hilarious)
-
-w = "This is the left side of..."
-e = "a string with a right side."
-
-print(w + e)
+print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
